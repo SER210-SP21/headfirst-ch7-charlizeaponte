ed25df9b146f7ceebf3ff94ddf4575d47f2ad7a5
charlizeaponte <charlizeaponte@yahoo.com>
2024-01-31 20:42:28 -0500

    Chapter 6 demo

---
+++ app/src/main/java/edu/quinnipiac/sercetmessage/MainActivity.kt
@@ -0,0 +1,17 @@
+/**
+ * @author Charlize Aponte
+ * 1/31/24
+ * Assignment: Demo from Chapter 6: Screen Message
+ * The program allows you to write a secret message by using navigation
+ */
+package edu.quinnipiac.sercetmessage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
